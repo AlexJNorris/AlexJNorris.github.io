bf8c15ae9b14c8b78518b61ec8f59cf8477b05eb
AlexJNorris <a.norris97@icloud.com>
2024-06-18 14:38:23 -0700

    add responsive iframes

--- react-portfolio/src/awards.js
+++ react-portfolio/src/awards.js
@@ -7,8 +7,10 @@ function Awards() {
             <div class="row">
                 <div style={{ margin: 0 + ' auto',  padding: 15 + "px"}} class="col text-center left-block green-block round-corners  bg-kinda-dark btn-margin-20">
                     <h1 class="display-4" style={{ fontSize: 2 + "rem" }}> Strategio Enterprise Simulator - Certificate of Completion </h1>
-                    <hr/>
-                    <iframe src="/StrategioCert.pdf#zoom=50" width="640" height="480" allow="autoplay"></iframe>
+                    <hr />
+                    <div className="icontainer">
+                        <iframe className="responsive-iframe" src="/StrategioCert.pdf" allow="autoplay"></iframe>
+                    </div>
                 </div>
             </div>
             <hr/>
@@ -16,7 +18,9 @@ function Awards() {
                     <div style={{ margin: 0 + ' auto', padding: 15 + "px" }} class="col text-center left-block green-block round-corners  bg-kinda-dark btn-margin-20">
                     <h1 class="display-4" style={{ fontSize: 2 + "rem" }}> Hack-A-Roo - Third Place Award </h1>
                     <hr/>
-                    <iframe src="/hack-a-roo_certificate.pdf#zoom=50" width="640" height="480" allow="autoplay"></iframe>
+                    <div className="icontainer">
+                        <iframe className="responsive-iframe" src="/hack-a-roo_certificate.pdf" allow="autoplay"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
--- react-portfolio/src/resume.js
+++ react-portfolio/src/resume.js
@@ -9,7 +9,9 @@ function Resume() {
             <div style={{ margin: 0 + ' auto', padding: 15 + 'px' }} class="col text-center left-block green-block round-corners  bg-kinda-dark btn-margin-20">
                 <h1 class="display-4" style={{fontSize: 2 + 'rem'}}> Resume </h1>
                 <hr />
-                <iframe src="https://drive.google.com/file/d/1qh5REW9KDFRAlPLrwhAQFOsl8r8Y_amd/preview" width="640" height="480" allow="autoplay"></iframe>
+                <div className="icontainer">
+                    <iframe className="responsive-iframe" src="https://drive.google.com/file/d/1qh5REW9KDFRAlPLrwhAQFOsl8r8Y_amd/preview" allow="autoplay"></iframe>
+                </div>
             </div>
         </div>
         )
